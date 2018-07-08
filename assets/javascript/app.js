a111b41a0fa106075982cc22ab5276374dc9ec50
Brucimus <Brucimus@gmail.com>
2018-07-08 00:18:07 -0700

    link everything

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDUSscaxURnRLcYCMYA38KklKcL8icp9qs",
+authDomain: "brucimus-testing-1.firebaseapp.com",
+databaseURL: "https://brucimus-testing-1.firebaseio.com",
+projectId: "brucimus-testing-1",
+storageBucket: "brucimus-testing-1.appspot.com",
+messagingSenderId: "978947948224"
+};
+
+firebase.initializeApp(config);
